f4f693234d56bba2f03c7a0338657376ee39a380
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-06-01 17:33:37 -0700

    added db query

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -9,11 +10,33 @@ app.use(express.json());
 
 
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'localhost',
+        password: 'Cisco007.',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+    );
+    
+    
+    db.query(`SELECT * FROM candidates`, (err, rows) => {
+        console.log(rows);
+    });
 
 // Deafualt response for any other request (Not found)
 app.use((req, res) => {
     res.status(404).end();
 });
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    })
+})
 
 
 app.listen(PORT, () => {
